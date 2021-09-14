6b1a300a45e2c450b806f19cb8c62c5d4666501f
vegexd <74072242+vegexd@users.noreply.github.com>
2021-09-14 19:04:33 +0300

    Add files via upload

---
+++ carhud-vege/README.md
@@ -0,0 +1,12 @@
+# Fivem tgiann-carhud script
+
+![IMG](https://i.imgur.com/MEQDWw4.png "IMG")
+
+**Credit**
+* SimpleCarHUD (https://github.com/bepo13/FiveM-SimpleCarHUD)
+* Compass and street name HUD (https://forum.cfx.re/t/release-compass-and-street-name-hud)
+
+**Requirements**
+* interact-sound (https://github.com/plunkettscott/interact-sound)
+
+Video: https://streamable.com/2s8q6y
---
+++ carhud-vege/config.lua
@@ -0,0 +1,17 @@
+TGIANN = {}
+TGIANN.useKm = true
+TGIANN.seatbeltDisableExit = true -- turvavyön kiinnilaitto ääni
+TGIANN.seatbeltPlaySound = true -- turvavyön kiinnitys ääni
+TGIANN.seatbeltPlayAlarmSound = true -- turvavyön hälytys ääni
+
+TGIANN.seatbeltInput = 311 -- [K] turvavyö päälle/ei
+TGIANN.cruiseInput = 246  -- [Y] vakionopeuden säädin ei/kyllä
+
+TGIANN.speedLimit = 80 
+TGIANN.seatbeltEjectSpeed = 45.0
+TGIANN.crashSeatbeltEjectSpeed = 165.0
+TGIANN.crashFlatTire = 80.0
+
+--location of the hud
+TGIANN.positionx = 0.0 
+TGIANN.positiony = 0.0 
